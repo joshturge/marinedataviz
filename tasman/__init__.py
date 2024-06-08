4f644fb195ae1a504b3760a9198368657f9c2c00
Joshua Turgeon <jturgeon@x1.turgeon.au>
2024-06-08 14:55:34 +1000

    Added plots of all sensors to frontend view

--- tasman/__init__.py
+++ tasman/__init__.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
 import xml.etree.ElementTree as et 
 from datetime import datetime
+import pandas as pd
+from matplotlib.figure import Figure
+from io import BytesIO
+import base64
 from flask import Flask, render_template, request
 
 @dataclass
@@ -92,6 +96,16 @@ def unmarshal_device(xmlroot: et.Element) -> Device:
 
     return device
 
+def get_dataframe(observs: list[Observation]):
+    time = []
+    values = []
+    for obs in observs:
+        time.append(obs.time)
+        values.append(obs.value)
+    dataframe = pd.DataFrame({'time': time, 'values': values})
+    dataframe.set_index('time', inplace=True)
+    return dataframe
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -106,7 +120,26 @@ def upload_file():
     xmlroot = et.fromstring(file.stream.read())
     device = unmarshal_device(xmlroot)
 
-    return render_template("device_stats.html", device=device), 200
+    plots = [] 
+    for sensor in device.sensors:
+        df = get_dataframe(sensor.observations)
+        fig = Figure()
+        ax = fig.subplots()
+        ax.plot(df.index, df.values)
+        if sensor.depth != 0:
+            ax.set_title(f"{sensor.name} at depth of {sensor.depth:.2f} metres")
+        else:
+            ax.set_title(f"{sensor.name}")
+        ax.tick_params(axis='x', labelrotation=30)
+        # Save it to a temporary buffer.
+        buf = BytesIO()
+        fig.savefig(buf, format="png")
+        # Embed the result in the html output.
+        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+        plots.append(data)
+
+
+    return render_template("device_stats.html", device=device, plots=plots), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
